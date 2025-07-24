bdd08615fb2d0743195e21eb4adef3b056f69238
SurDno <surdno@yandex.ru>
2025-07-24 23:37:33 +0100

    Added logging for when instruction is not copied correctly

--- P2ModLoader/Patching/Assembly/ILCloning/InstructionCloner.cs
+++ P2ModLoader/Patching/Assembly/ILCloning/InstructionCloner.cs
@@ -1,12 +1,29 @@
 using Mono.Cecil;
 using Mono.Cecil.Cil;
+using P2ModLoader.Logging;
 
 namespace P2ModLoader.Patching.Assembly.ILCloning;
 
 public static class InstructionCloner {
-    public static Instruction CloneInstruction(Instruction src, ModuleDefinition targetModule,
+
+    public static Instruction CloneInstruction(Instruction src, ModuleDefinition module,
         Map<VariableDefinition> variableMap, Map<ParameterDefinition> parameterMap, Map<Instruction> instructionMap,
-        IGenericParameterProvider contextProvider, TypeDefinition currentType) { 	
+        IGenericParameterProvider contextProvider, TypeDefinition type) { 	
+        var clone = CloneInstructionImpl(src, module, variableMap, parameterMap, instructionMap, contextProvider, type);
+        
+        var srcText = $"{src.OpCode} “{src.Operand}” ({src.Operand?.GetType().Name})";
+        var cloneText = $"{clone.OpCode} “{clone.Operand}” ({clone.Operand?.GetType().Name})";
+        if (srcText == cloneText) return clone;
+        Logger.Log(LogLevel.Error, $"Instruction mismatch after cloning! Some data may be lost!");
+        Logger.Log(LogLevel.Error, $"[BEFORE CLONE] IL_{src.Offset:X4}: {srcText}");
+        Logger.Log(LogLevel.Error, $"[AFTER  CLONE] IL_{clone.Offset:X4}: {cloneText}");
+
+        return clone;
+    }
+    
+    private static Instruction CloneInstructionImpl(Instruction src, ModuleDefinition module,
+        Map<VariableDefinition> variableMap, Map<ParameterDefinition> parameterMap, Map<Instruction> instructionMap,
+        IGenericParameterProvider contextProvider, TypeDefinition type) { 	
         var opcode = src.OpCode;
         var operand = src.Operand;
 
@@ -14,40 +31,40 @@ public static class InstructionCloner {
             case null:
                 return Instruction.Create(opcode);
             case GenericInstanceMethod gim:
-                var defRef = targetModule.ImportReference(gim.ElementMethod);
+                var defRef = module.ImportReference(gim.ElementMethod);
                 var inst = new GenericInstanceMethod(defRef);
                 foreach (var arg in gim.GenericArguments) {
                     if (arg is GenericParameter gp)
                         inst.GenericArguments.Add(defRef.GenericParameters[gp.Position]);
                     else
-                        inst.GenericArguments.Add(targetModule.ImportReference(arg));
+                        inst.GenericArguments.Add(module.ImportReference(arg));
                 }
                 return Instruction.Create(opcode, inst);
             case GenericInstanceType git:
-                var typeReference = targetModule.ImportReference(git.ElementType);
+                var typeReference = module.ImportReference(git.ElementType);
                 var instance = new GenericInstanceType(typeReference);
                 foreach (var arg in git.GenericArguments) {
                     if (arg is GenericParameter gp)
                         instance.GenericArguments.Add(typeReference.GenericParameters[gp.Position]);
                     else
-                        instance.GenericArguments.Add((TypeReference)targetModule.ImportReference(arg));
+                        instance.GenericArguments.Add((TypeReference)module.ImportReference(arg));
                 }
                 return Instruction.Create(opcode, instance);
             case GenericParameter gp:
                 var mapped = contextProvider.GenericParameters[gp.Position];
                 return Instruction.Create(opcode, mapped);
             case TypeReference typeRef:
-                return Instruction.Create(opcode, targetModule.ImportReference(typeRef));
+                return Instruction.Create(opcode, module.ImportReference(typeRef));
             case MethodReference methodRef:
-                if (methodRef.DeclaringType.FullName != currentType.FullName) {
+                if (methodRef.DeclaringType.FullName != type.FullName) {
                     try {
-                        return Instruction.Create(opcode, targetModule.ImportReference(methodRef, currentType));
+                        return Instruction.Create(opcode, module.ImportReference(methodRef, type));
                     } catch {
-                        return Instruction.Create(opcode, targetModule.ImportReference(methodRef));
+                        return Instruction.Create(opcode, module.ImportReference(methodRef));
                     }
                 }
 
-                var candidates = currentType.Methods
+                var candidates = type.Methods
                     .Where(m => m.Name == methodRef.Name && m.Parameters.Count == methodRef.Parameters.Count);
                 var resolvedMethod = candidates.FirstOrDefault(m =>
                     m.Parameters.Select(p => new {
@@ -61,12 +78,12 @@ public static class InstructionCloner {
                             })
                         )
                 );
-                return Instruction.Create(opcode, targetModule.ImportReference(resolvedMethod ?? methodRef));
+                return Instruction.Create(opcode, module.ImportReference(resolvedMethod ?? methodRef));
             case FieldReference fieldRef:
-                if (fieldRef.DeclaringType.FullName != currentType.FullName)
-                    return Instruction.Create(opcode, targetModule.ImportReference(fieldRef));
-                var originalField = currentType.Fields.FirstOrDefault(f => f.Name == fieldRef.Name);
-                return Instruction.Create(opcode, targetModule.ImportReference(originalField ?? fieldRef));
+                if (fieldRef.DeclaringType.FullName != type.FullName)
+                    return Instruction.Create(opcode, module.ImportReference(fieldRef));
+                var originalField = type.Fields.FirstOrDefault(f => f.Name == fieldRef.Name);
+                return Instruction.Create(opcode, module.ImportReference(originalField ?? fieldRef));
             case string strOperand:
                 return Instruction.Create(opcode, strOperand);
             case sbyte sbyteOperand:
@@ -92,14 +109,14 @@ public static class InstructionCloner {
             case ParameterDefinition parameter:
                 return Instruction.Create(opcode, parameterMap[parameter]);
             case CallSite callSite: 
-                var newCallSite = new CallSite(targetModule.ImportReference(callSite.ReturnType)) {
+                var newCallSite = new CallSite(module.ImportReference(callSite.ReturnType)) {
                     CallingConvention = callSite.CallingConvention,
                     HasThis = callSite.HasThis,
                     ExplicitThis = callSite.ExplicitThis
                 };
 
                 foreach (var newParam in callSite.Parameters.Select(p =>
-                             new ParameterDefinition(targetModule.ImportReference(p.ParameterType)))) {
+                             new ParameterDefinition(module.ImportReference(p.ParameterType)))) {
                     newCallSite.Parameters.Add(newParam);
                 }
 
